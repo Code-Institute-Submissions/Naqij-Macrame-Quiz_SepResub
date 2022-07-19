e0e0cd45b67d4b64fa80d16415d3ad20f51ef613
Naqij <naqi.jaffari@gmail.com>
2022-07-19 21:14:26 +0000

    changed name of the files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,5 @@
 //https://opentdb.com/api.php?amount=10&category=25&type=multiple
 
-async function loadQuestion(){
-    const APIUrl = 'https://opentdb.com/api.php?amount=10&category=25&type=multiple';
-    const result = await fetch(`${APIUrl}`);
-    const data = await result.json();
-    console.lod(data);
-}
-
-loadQuestion();
+document.addEventListener("DOMContentLoaded", function(){
+    
+})
