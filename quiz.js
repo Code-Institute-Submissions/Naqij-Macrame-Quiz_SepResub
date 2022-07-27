63abd31f20e054db6a9d22755855fcd6d314f372
Naqij <naqi.jaffari@gmail.com>
2022-07-27 18:41:03 +0000

    setting up as 1 page code

--- quiz.js
+++ quiz.js
@@ -2,59 +2,14 @@ const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
+const gameRef = document.querySelector('#game');
+const homeRef = document.querySelector('#home');
+const playBtnRef = document.querySelector('#play-btn');
 
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
-let availableQuestions = []
-
-let questions = [
-    {
-        question: "What area did Macramé originated from?",
-        choice1: "Africa",
-        choice2: "Arab",
-        choice3: "Asia",
-        choice4: "America",
-        answer: 2,
-    },
-
-    {
-        question: "What type of craft is Macramé?",
-        choice1: "Paper craft",
-        choice2: "Stone craft",
-        choice3: "Fiber art",
-        choice4: "All the above",
-        answer: 3,
-    },
-
-    {
-        question: "Macramé can be used to make?",
-        choice1: "Plant hangers",
-        choice2: "Jewellery",
-        choice3: "Clothing",
-        choice4: "All the above",
-        answer: 4,
-    },
-
-    {
-        question: "Which of the following is NOT a type of Macramé knot?",
-        choice1: "Larks head",
-        choice2: "Spiral Knot",
-        choice3: "Cordinal beak knot",
-        choice4: "Clove hitch",
-        answer: 3,
-    },
-
-    {
-        question: "According to the wonder, why did sailors practice Macramé on their ships?",
-        choice1: "To help pass the time",
-        choice2: "To mend their torn cloths",
-        choice3: "To repair holes in the ships sail",
-        choice4: "To create containers for their cargo",
-        answer: 1,
-    }
-]
 
 const SCORE_POINTS = 20
 const MAX_QUESTIONS = 5
@@ -62,8 +17,10 @@ const MAX_QUESTIONS = 5
 startGame = () => {
     questionCounter = 0
     score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
+    const quiz = fetch("assets/data/questions.json").then(res => res.json()).then(data => data)
+    .then(res => res.json())
+    .then(data => data)
+    getNewQuestion(quiz.questions)
 }
 
 getNewQuestion = () => {
@@ -119,4 +76,8 @@ incrementScore = num => {
     scoreText.innerText = score
 }
 
-startGame()
+playBtnRef.addEventListener("click", () => {
+    gameRef.classList.remove("hidden")
+    homeRef.classList.add("hidden")
+    startGame()
+})
