fc32e225bc4f998d339f3ef5a09ade51cb2082e7
Naqij <naqi.jaffari@gmail.com>
2022-09-21 18:25:17 +0000

    Added html for the quiz

--- assets/js/highscore.js
+++
@@ -1,7 +0,0 @@
-const highScoresList = document.querySelector('#highScoresList');
-
-
-highScoresList.innerHTML =
-highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join("")
